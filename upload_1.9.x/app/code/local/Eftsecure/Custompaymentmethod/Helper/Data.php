153df1e7c282b0ca73b3811f2addb65e54503863
Krunal Sathavara <krunal9206@gmail.com>
2017-02-15 10:12:27 +0530

    Add files via upload

--- upload_1.9.x/app/code/local/Eftsecure/Custompaymentmethod/Helper/Data.php
+++ upload_1.9.x/app/code/local/Eftsecure/Custompaymentmethod/Helper/Data.php
@@ -1,1 +1,9 @@
-
+<?php
+// app/code/local/Envato/Custompaymentmethod/Helper/Data.php
+class Eftsecure_Custompaymentmethod_Helper_Data extends Mage_Core_Helper_Abstract
+{
+  function getPaymentGatewayUrl() 
+  {
+    return Mage::getUrl('custompaymentmethod/payment/gateway', array('_secure' => false));
+  }
+}
