0acc6794c03ba57899f478f7a0ce92809fc69c8a
Krunal Sathavara <krunal9206@gmail.com>
2017-02-15 10:32:40 +0530

    Add files via upload

--- upload_1.9.x/app/code/local/Eftsecure/Custompaymentmethod/Block/Form/Custompaymentmethod.php
+++ upload_1.9.x/app/code/local/Eftsecure/Custompaymentmethod/Block/Form/Custompaymentmethod.php
@@ -1,1 +1,9 @@
-
+<?php
+class Eftsecure_Custompaymentmethod_Block_Form_Custompaymentmethod extends Mage_Payment_Block_Form
+{
+  protected function _construct()
+  {
+    parent::_construct();
+    $this->setTemplate('custompaymentmethod/form/info.phtml');
+  }
+}
