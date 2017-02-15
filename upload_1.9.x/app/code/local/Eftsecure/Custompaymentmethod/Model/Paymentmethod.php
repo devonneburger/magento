e05322c8dad6fe11ebed738a4cbb30e3d26d62eb
Krunal Sathavara <krunal9206@gmail.com>
2017-02-15 10:04:17 +0530

    Add files via upload

--- upload_1.9.x/app/code/local/Eftsecure/Custompaymentmethod/Model/Paymentmethod.php
+++ upload_1.9.x/app/code/local/Eftsecure/Custompaymentmethod/Model/Paymentmethod.php
@@ -1,1 +1,12 @@
-
+<?php
+class Eftsecure_Custompaymentmethod_Model_Paymentmethod extends Mage_Payment_Model_Method_Abstract {
+  protected $_code  = 'custompaymentmethod';
+  protected $_formBlockType = 'custompaymentmethod/form_custompaymentmethod';
+  //protected $_infoBlockType = 'payment/info';
+ 
+  public function getOrderPlaceRedirectUrl()
+  {
+	Mage::getSingleton('core/session')->setEftsecureFlag(1);
+    return Mage::getUrl('custompaymentmethod/payment/redirect', array('_secure' => false));
+  }
+}
