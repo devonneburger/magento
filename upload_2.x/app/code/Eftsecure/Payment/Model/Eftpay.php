50b4717742f54dbcdbae10efe75cf36a32349441
Krunal Sathavara <krunal9206@gmail.com>
2017-02-18 18:35:27 +0530

    Add files via upload

--- upload_2.x/app/code/Eftsecure/Payment/Model/Eftpay.php
+++ upload_2.x/app/code/Eftsecure/Payment/Model/Eftpay.php
@@ -1,1 +1,16 @@
+<?php
 
+namespace Eftsecure\Payment\Model;
+
+class Eftpay extends \Magento\Payment\Model\Method\AbstractMethod
+{
+
+    protected $_code = 'eftpay';
+
+    protected $_isOffline = true;
+
+	public function getInstructions()
+    {
+        return trim($this->getConfigData('instructions'));
+    }
+}
