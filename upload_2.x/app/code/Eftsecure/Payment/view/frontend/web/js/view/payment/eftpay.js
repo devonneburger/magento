a5ae62e680f41c6114341651908a7dc181660f44
Krunal Sathavara <krunal9206@gmail.com>
2017-02-18 18:43:27 +0530

    Add files via upload

--- upload_2.x/app/code/Eftsecure/Payment/view/frontend/web/js/view/payment/eftpay.js
+++ upload_2.x/app/code/Eftsecure/Payment/view/frontend/web/js/view/payment/eftpay.js
@@ -1,1 +1,20 @@
-
+define(
+    [
+        'uiComponent',
+        'Magento_Checkout/js/model/payment/renderer-list'
+    ],
+    function (
+        Component,
+        rendererList
+    ) {
+        'use strict';
+        rendererList.push(
+            {
+                type: 'eftpay',
+                component: 'Eftsecure_Payment/js/view/payment/method-renderer/eftpay-method'
+            }
+        );
+        /** Add view logic here if needed */
+        return Component.extend({});
+    }
+);
